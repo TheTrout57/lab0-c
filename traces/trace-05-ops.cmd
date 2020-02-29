5889edc90692e0471cf41a02d5b110b983469ecb
Ming-Yan Chiang <s93294@gmail.com>
2020-02-29 17:54:02 +0800

    Initial commit

---
+++ traces/trace-05-ops.cmd
@@ -0,0 +1,24 @@
+# Test of insert_head, insert_tail, remove_head, reverse, size, and sort
+option fail 0
+option malloc 0
+new
+ih dolphin
+ih bear
+ih gerbil
+reverse
+size
+it meerkat
+it bear
+it gerbil
+size
+rh dolphin
+reverse
+size
+sort
+rh bear
+rh bear
+rh gerbil
+rh gerbil
+rh meerkat
+size
+free
